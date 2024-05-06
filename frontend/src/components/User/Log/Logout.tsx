d1ec4804209a90f23a8b96472a056618e296e6be
Prashant Srivastav <srivastavp891@gmail.com>
2024-05-06 21:32:36 +0530

    modal logout

--- frontend/src/components/User/Log/Logout.tsx
+++ frontend/src/components/User/Log/Logout.tsx
@@ -4,20 +4,35 @@ import { ThemeContext } from "../../context/ThemeContext";
 import { useNavigate } from "react-router-dom";
 
 const Logout = () => {
-  const { authProps } = React.useContext(ThemeContext);
+  const { authProps, modalsProps } = React.useContext(ThemeContext);
+
   const navigate = useNavigate();
 
   const handleLogout = () => {
     authProps.setIsLogged(false);
     authProps.setToken("");
     sessionStorage.clear();
+    modalsProps.setIsOpenModal(false)
     navigate("/");
+  };
+
+  const OpenModel = () => {
+    modalsProps.setIsOpenModal(true);
+    modalsProps.setModalValue(
+      <>
+        <h1>Are you sure you want to Logout?</h1>
+        <div className="d-flex justify-content-evenly">
+          <button className="btn btn-danger" onClick={handleLogout}>LOGOUT</button>
+          <button className="btn btn-success" onClick={(e)=>modalsProps.setIsOpenModal(false)}>CANCEL</button>
+        </div>
+      </>
+    );
   };
 
   return (
     <button
       className="btn bg-gradient text-white rounded-5 "
-      onClick={handleLogout}
+      onClick={OpenModel}
     >
       Logout
     </button>
